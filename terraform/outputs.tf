63f7a2ad0c4ae75ee2576cd9eb53c9de7792a6d2
Зимин Николай <Nikola.Zimin@x5.ru>
2026-02-17 09:35:56 +0300

    s4

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -3,19 +3,27 @@ output "kafka_cluster_id" {
   value       = yandex_mdb_kafka_cluster.kafka_cluster.id
 }
 
-output "kafka_bootstrap_servers" {
-  description = "Kafka bootstrap servers (internal)"
-  value       = yandex_mdb_kafka_cluster.kafka_cluster.config[0].kafka[0].internal_endpoint
+output "kafka_hosts" {
+  description = "List of Kafka hosts (FQDN)"
+  value = [
+    for h in yandex_mdb_kafka_cluster.kafka_cluster.host : h.name
+  ]
 }
 
-output "kafka_external_bootstrap_servers" {
-  description = "Kafka bootstrap servers (external/public)"
-  value       = yandex_mdb_kafka_cluster.kafka_cluster.config[0].kafka[0].external_endpoint
+output "kafka_public_hosts" {
+  description = "Kafka hosts with public IP"
+  value = [
+    for h in yandex_mdb_kafka_cluster.kafka_cluster.host : h.name
+    if h.assign_public_ip
+  ]
 }
 
-output "schema_registry_url" {
-  description = "Schema Registry URL"
-  value       = yandex_mdb_kafka_cluster.kafka_cluster.config[0].schema_registry_endpoint
+output "schema_registry_urls" {
+  description = "Schema Registry URLs (HTTPS on 443)"
+  value = [
+    for h in yandex_mdb_kafka_cluster.kafka_cluster.host :
+    "https://${h.name}:443"
+  ]
 }
 
 output "dataproc_cluster_id" {
@@ -23,12 +31,10 @@ output "dataproc_cluster_id" {
   value       = yandex_dataproc_cluster.hadoop_cluster.id
 }
 
-output "dataproc_master_public_ip" {
-  description = "DataProc master public IP"
-  value       = yandex_dataproc_cluster.hadoop_cluster.master_external_ip
-}
-
-output "hadoop_web_ui" {
-  description = "Hadoop Web UI URL"
-  value       = "http://${yandex_dataproc_cluster.hadoop_cluster.master_external_ip}:8088"
+output "dataproc_public_hosts" {
+  description = "DataProc hosts with public IP"
+  value = [
+    for h in yandex_dataproc_cluster.hadoop_cluster.host : h.name
+    if h.assign_public_ip
+  ]
 }

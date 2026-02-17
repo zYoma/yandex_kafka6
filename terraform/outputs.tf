6aa037cbcd1cb8943ad5507b766bd9e9b1347377
Зимин Николай <Nikola.Zimin@x5.ru>
2026-02-17 09:34:23 +0300

    s4

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -4,15 +4,18 @@ output "kafka_cluster_id" {
 }
 
 output "kafka_bootstrap_servers" {
-  description = "Kafka bootstrap servers"
-  value       = join(",", [
-    for b in yandex_mdb_kafka_cluster.kafka_cluster.config[0].kafka[0].brokers : "${b.hostname}:9091"
-  ])
+  description = "Kafka bootstrap servers (internal)"
+  value       = yandex_mdb_kafka_cluster.kafka_cluster.config[0].kafka[0].internal_endpoint
+}
+
+output "kafka_external_bootstrap_servers" {
+  description = "Kafka bootstrap servers (external/public)"
+  value       = yandex_mdb_kafka_cluster.kafka_cluster.config[0].kafka[0].external_endpoint
 }
 
 output "schema_registry_url" {
   description = "Schema Registry URL"
-  value       = "https://${yandex_mdb_kafka_cluster.kafka_cluster.config[0].schema_registry[0].external_endpoint}"
+  value       = yandex_mdb_kafka_cluster.kafka_cluster.config[0].schema_registry_endpoint
 }
 
 output "dataproc_cluster_id" {
@@ -20,12 +23,12 @@ output "dataproc_cluster_id" {
   value       = yandex_dataproc_cluster.hadoop_cluster.id
 }
 
-output "dataproc_master_ip" {
+output "dataproc_master_public_ip" {
   description = "DataProc master public IP"
-  value = yandex_dataproc_cluster.hadoop_cluster.cluster_config[0].subcluster_spec[0].hosts[0].assign_public_ip
+  value       = yandex_dataproc_cluster.hadoop_cluster.master_external_ip
 }
 
 output "hadoop_web_ui" {
   description = "Hadoop Web UI URL"
-  value       = "http://${yandex_dataproc_cluster.hadoop_cluster.cluster_config[0].subcluster_spec[0].hosts[0].hostname}:8088"
+  value       = "http://${yandex_dataproc_cluster.hadoop_cluster.master_external_ip}:8088"
 }

9880b6e24fc632fb1d1bcaa21719b09d54df504c
Зимин Николай <Nikola.Zimin@x5.ru>
2026-02-17 09:18:07 +0300

    s3

--- terraform/main.tf
+++ terraform/main.tf
@@ -9,10 +9,40 @@ terraform {
 
 provider "yandex" {}
 
-locals {
-  folder_id    = var.folder_id
-  zone         = var.zone
+########################################
+# Variables
+########################################
+
+variable "folder_id" {
+  type = string
 }
+
+variable "zone" {
+  type    = string
+  default = "ru-central1-a"
+}
+
+variable "kafka_user" {
+  type = string
+}
+
+variable "kafka_password" {
+  type      = string
+  sensitive = true
+}
+
+########################################
+# Locals
+########################################
+
+locals {
+  folder_id = var.folder_id
+  zone      = var.zone
+}
+
+########################################
+# Network
+########################################
 
 resource "yandex_vpc_network" "kafka_network" {
   name = "kafka-network"
@@ -25,10 +55,72 @@ resource "yandex_vpc_subnet" "kafka_subnet" {
   v4_cidr_blocks = ["10.0.0.0/24"]
 }
 
+########################################
+# Security Group
+########################################
+
+resource "yandex_vpc_security_group" "kafka_sg" {
+  name       = "kafka-security-group"
+  network_id = yandex_vpc_network.kafka_network.id
+
+  # внутренний трафик
+  ingress {
+    protocol       = "ANY"
+    description    = "Allow internal traffic"
+    from_port      = 0
+    to_port        = 65535
+    v4_cidr_blocks = ["10.0.0.0/24"]
+  }
+
+  # Kafka SSL
+  ingress {
+    protocol       = "TCP"
+    description    = "Kafka SSL"
+    from_port      = 9091
+    to_port        = 9091
+    v4_cidr_blocks = ["10.0.0.0/24"]
+  }
+
+  # SSH
+  ingress {
+    protocol       = "TCP"
+    description    = "SSH"
+    from_port      = 22
+    to_port        = 22
+    v4_cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  # Schema Registry (HTTPS)
+  ingress {
+    protocol       = "TCP"
+    description    = "Schema Registry"
+    from_port      = 443
+    to_port        = 443
+    v4_cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    protocol       = "ANY"
+    description    = "Allow all outbound"
+    from_port      = 0
+    to_port        = 65535
+    v4_cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+########################################
+# Kafka Cluster
+########################################
+
 resource "yandex_mdb_kafka_cluster" "kafka_cluster" {
   name        = "kafka-cluster"
-  environment = "PRODUCTION"
+  folder_id   = local.folder_id
+  environment = "PRESTABLE"
   network_id  = yandex_vpc_network.kafka_network.id
+
+  security_group_ids = [
+    yandex_vpc_security_group.kafka_sg.id
+  ]
 
   kafka {
     resources {
@@ -49,22 +141,8 @@ resource "yandex_mdb_kafka_cluster" "kafka_cluster" {
     enabled = true
   }
 
-  zones = [local.zone]
-
+  zones         = [local.zone]
   brokers_count = 3
-
-  host {
-    zone       = local.zone
-    subnet_id  = yandex_vpc_subnet.kafka_subnet.id
-    assign_public_ip = false
-  }
-
-  host {
-    zone            = local.zone
-    subnet_id       = yandex_vpc_subnet.kafka_subnet.id
-    assign_public_ip = true
-    type            = "SCHEMA_REGISTRY"
-  }
 
   user {
     name     = var.kafka_user
@@ -72,9 +150,10 @@ resource "yandex_mdb_kafka_cluster" "kafka_cluster" {
   }
 
   topic {
-    name             = "test-topic"
-    partitions       = 3
+    name               = "test-topic"
+    partitions         = 3
     replication_factor = 3
+
     topic_config {
       cleanup_policy = "delete"
       retention_ms   = 259200000
@@ -83,13 +162,37 @@ resource "yandex_mdb_kafka_cluster" "kafka_cluster" {
   }
 }
 
-resource "yandex_dataproc_cluster" "hadoop_cluster" {
-  name        = "hadoop-cluster"
-  folder_id   = local.folder_id
-  description = "Hadoop HDFS для интеграции с Kafka"
-  service_account_id = yandex_iam_service_account.dataproc_sa.id
+########################################
+# DataProc Service Account
+########################################
 
-  zone_id = local.zone
+resource "yandex_iam_service_account" "dataproc_sa" {
+  name = "dataproc-service-account"
+}
+
+resource "yandex_resourcemanager_folder_iam_member" "dataproc_agent" {
+  folder_id = local.folder_id
+  member    = "serviceAccount:${yandex_iam_service_account.dataproc_sa.id}"
+  role      = "dataproc.agent"
+}
+
+resource "yandex_resourcemanager_folder_iam_member" "dataproc_provisioner" {
+  folder_id = local.folder_id
+  member    = "serviceAccount:${yandex_iam_service_account.dataproc_sa.id}"
+  role      = "dataproc.provisioner"
+}
+
+########################################
+# DataProc Cluster
+########################################
+
+resource "yandex_dataproc_cluster" "hadoop_cluster" {
+  name                = "hadoop-cluster"
+  folder_id           = local.folder_id
+  description         = "Hadoop HDFS для интеграции с Kafka"
+  service_account_id  = yandex_iam_service_account.dataproc_sa.id
+  zone_id             = local.zone
+  security_group_ids  = [yandex_vpc_security_group.kafka_sg.id]
 
   config {
     version_id = "2.1.1"
@@ -109,9 +212,7 @@ resource "yandex_dataproc_cluster" "hadoop_cluster" {
       }
 
       hosts_count = 1
-
       subnet_id   = yandex_vpc_subnet.kafka_subnet.id
-
       assign_public_ip = true
     }
 
@@ -126,75 +227,8 @@ resource "yandex_dataproc_cluster" "hadoop_cluster" {
       }
 
       hosts_count = 2
-
       subnet_id   = yandex_vpc_subnet.kafka_subnet.id
-
       assign_public_ip = false
     }
-  }
-
-  security_group_ids = [yandex_vpc_security_group.kafka_sg.id]
-}
-
-resource "yandex_iam_service_account" "dataproc_sa" {
-  name = "dataproc-service-account"
-}
-
-resource "yandex_resourcemanager_folder_iam_member" "dataproc_sa" {
-  folder_id = local.folder_id
-  member    = "serviceAccount:${yandex_iam_service_account.dataproc_sa.id}"
-  role      = "dataproc.agent"
-}
-
-resource "yandex_vpc_security_group" "kafka_sg" {
-  name        = "kafka-security-group"
-  network_id  = yandex_vpc_network.kafka_network.id
-
-  ingress {
-    protocol       = "ANY"
-    description    = "Allow internal traffic"
-    from_port      = 0
-    to_port        = 65535
-    v4_cidr_blocks = ["10.0.0.0/24"]
-  }
-
-  ingress {
-    protocol       = "TCP"
-    description    = "SSH"
-    from_port      = 22
-    to_port        = 22
-    v4_cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  ingress {
-    protocol       = "TCP"
-    description    = "Schema Registry"
-    from_port      = 443
-    to_port        = 443
-    v4_cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  ingress {
-    protocol       = "TCP"
-    description    = "DataProc SSH proxy"
-    from_port      = 50105
-    to_port        = 50105
-    v4_cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  ingress {
-    protocol       = "TCP"
-    description    = "Hadoop Web UI (YARN, NameNode)"
-    from_port      = 8070
-    to_port        = 9870
-    v4_cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  egress {
-    protocol       = "ANY"
-    description    = "Allow all outbound"
-    from_port      = 0
-    to_port        = 65535
-    v4_cidr_blocks = ["0.0.0.0/0"]
   }
 }
